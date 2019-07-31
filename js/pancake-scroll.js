6f9315f8eb6078157730ffb7ef6fcaadcd8e5a74
Tam-piriye <52682057+Tam-piriye@users.noreply.github.com>
2019-07-31 13:55:37 +0100

    updating thw whole folder
    Adding photos.
    Worked on the html from a branch
     merged all the changes I made.

---
+++ js/pancake-scroll.js
@@ -0,0 +1,32 @@
+
+
+$(function(){
+	$('#recipe-pc').slimScroll({
+		height: 'auto'
+	});
+});
+
+
+$('#recipe-pc').slimScroll({
+	 height: 'auto'
+			  });
+
+
+
+
+
+
+
+
+
+//
+// <script>
+// function myFunction() {
+//   var x = document.getElementByCLASS("btn btn-sm btn-outline-secondary");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+// </script>
